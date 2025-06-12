fa05437a14cdbcaad6b6b0a585b766f1b6c54920
SafeeUrRehman65 <safee_rehman2001@hotmail.com>
2025-06-12 12:57:57 +0500

    New fresh and final commits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { DataContext, DialogueContext } from "../components/Context";
 import { SourceDestinationflag } from "../components/Context";
 import { useRef } from "react";
-
+import Footer from '../components/Footer.jsx'
 export default function Home() {
 
   const [modifier, setmodifier] = useState(null);
@@ -18,19 +18,19 @@ export default function Home() {
         <DialogueContext.Provider value={{ dialogShown }}>
           <div >
             <Navbar />
-            <div className="px-28 mt-12 h-[70vh]">
+            <div className="px-28 mt-12 h-max">
               <div className="rounded-md">
                 <div className="title">
                   <p className="text-black font-semibold text-lg">Sources - Destinations - Pipelines</p>
                   <p className="text-sm text-neutral-500 font-normal">View and manage your configured sources, destinations and pipelines</p>
                 </div>
-                <div className="mt-32 h-max border">
+                {/* <div className="mt-32 h-max border">
                   <div className="categories flex justify-between items-center w-1/4 h-[8vh] text-sm text-gray-900">
                     <div><p>Sources</p></div>
                     <div><p>Destinations</p></div>
                     <div><p>Pipelines</p></div>
                   </div>
-                  {/* <div className="tracker h-[3px] bg-blue-700 rounded-full"></div> */}
+                  <div className="tracker h-[3px] bg-blue-700 rounded-full"></div>
                 </div>
 
                 <div>
@@ -42,7 +42,7 @@ export default function Home() {
                     <p className="text-sm text-neutral-500">Database name</p>
                     <p className="text-sm text-neutral-500">Active status</p>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
             <div className="flex justify-center mt-8">
@@ -52,6 +52,7 @@ export default function Home() {
                 <Homebox title="Pipeline" />
               </div>
             </div>
+            <Footer />
           </div>
           {/* <DialogueBox /> */}
 
